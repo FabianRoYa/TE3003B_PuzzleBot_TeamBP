ce821df5602f3d1daecf7cac925673cc1d2f7280
Fabian Erubiel Rojas Yanez <A01706636@tec.mx>
2025-04-28 18:14:04 -0600

    Correction in setup.py

---
+++ launch/blackpearls_launch.py
@@ -0,0 +1,49 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, LogInfo
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('blackpearls_nav2_puzzlebot')
+    # Ruta al archivo del mundo de Gazebo 
+    world_file = os.path.join(
+        pkg_share,
+        'worlds',
+        'modulo3_world'
+    )
+
+    # Lanzar Gazebo con el mundo definido
+    gazebo = ExecuteProcess(
+        cmd=['gazebo', '--verbose', world_file, '-s', 'libgazebo_ros_factory.so'],
+        output='screen'
+    )
+
+
+    # Lanzar RViz para mapeo
+    rviz_mapping = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz_mapping',
+        arguments=['-d', mapping_rviz_config],
+        output='screen'
+    )
+
+    # Lanzar RViz para navegación
+    rviz_navigation = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz_navigation',
+        arguments=['-d', navigation_rviz_config],
+        output='screen'
+    )
+
+    ld = LaunchDescription([
+        LogInfo(msg=['Iniciando Gazebo con: ', world_file]),
+        gazebo,
+        LogInfo(msg=['Iniciando RViz (mapeo): ', mapping_rviz_config]),
+        rviz_mapping,
+        LogInfo(msg=['Iniciando RViz (navegación): ', navigation_rviz_config]),
+        rviz_navigation
+    ])
+    return ld
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'blackpearls_nav2_puzzlebot'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yma]*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
