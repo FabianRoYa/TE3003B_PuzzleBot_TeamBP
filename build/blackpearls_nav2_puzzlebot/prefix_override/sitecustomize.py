c5b45c5ae467def4b7921d9e3d2fd32a3004c038
Mirabay <tonyshauder@gmail.com>
2025-04-30 15:26:36 -0600

    puzzlebot Simulation

--- build/blackpearls_nav2_puzzlebot/prefix_override/sitecustomize.py
+++ build/blackpearls_nav2_puzzlebot/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/faroya/Desktop/ros2_ws/src/blackpearls_nav2_puzzlebot/install/blackpearls_nav2_puzzlebot'
+    sys.prefix = sys.exec_prefix = '/home/tony/PerlasNegras/install/blackpearls_nav2_puzzlebot'
