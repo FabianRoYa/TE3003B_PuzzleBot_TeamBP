796a53e9018b65ad4d0a76e7455ebf0535d376d5
a01704231 <112184077+a01704231@users.noreply.github.com>
2025-05-08 23:27:47 -0600

    Actividad M3.4: Simulación proyecto final corrección 2

---
+++ blackpearls_nav2_puzzlebot/launch/spawn_simple_launch.py
@@ -0,0 +1,93 @@
+# ODIO A SERGIO  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O  O D I O
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    package_name = 'blackpearls_nav2_puzzlebot'
+    rviz_config_file_path = 'rviz/urdf_gazebo_config.rviz' 
+    urdf_file_path = 'urdf/puzzlebot.urdf'
+    pkg_share = get_package_share_directory('blackpearls_nav2_puzzlebot')
+    world_path = os.path.join(pkg_share, 'worlds', 'world.world')
+    urdf_path = os.path.join(pkg_share, urdf_file_path)
+    rviz_config_path = os.path.join(pkg_share, rviz_config_file_path)
+
+    set_ign_path = SetEnvironmentVariable(
+        name='IGN_GAZEBO_RESOURCE_PATH',
+        value=os.path.dirname(pkg_share)
+    )
+
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'),
+                'launch', 'gz_sim.launch.py'
+            )
+        ),
+        launch_arguments={'gz_args': world_path}.items()
+    )
+
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=[
+            '-topic', 'robot_description',
+            '-name',  'puzzlebot',
+            '-x',     '0.3',
+            '-y',     '0.3',
+            '-z',     '0.0',
+            '-roll',  '0.0',
+            '-pitch', '0.0',
+            '-yaw',   '1.0'
+        ]
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', urdf_path]), 'use_sim_time': True}]
+    )
+
+    static_tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom']
+    )
+
+    frame_node = Node(
+        package=package_name,
+        executable='frame'
+    )
+
+    publish_wr_wl = Node(
+        package=package_name,
+        executable='publish_wr_wl',
+        parameters=[{'use_sim_time': True}],
+        emulate_tty=True,
+        output='screen'
+    )
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_config_path],
+        parameters=[{'use_sim_time': True}],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        set_ign_path,
+        gazebo_launch,
+        spawn_robot,
+        robot_state_publisher,
+        static_tf,
+        frame_node,
+        publish_wr_wl,
+        rviz
+    ])
