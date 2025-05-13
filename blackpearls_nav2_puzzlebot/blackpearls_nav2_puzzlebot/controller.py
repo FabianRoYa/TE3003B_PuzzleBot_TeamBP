6239ef4f39e4f440f71ebe9fa72fcfae45d1a98a
a01704231 <112184077+a01704231@users.noreply.github.com>
2025-05-13 17:08:03 -0600

    Update controller.py

--- blackpearls_nav2_puzzlebot/blackpearls_nav2_puzzlebot/controller.py
+++ blackpearls_nav2_puzzlebot/blackpearls_nav2_puzzlebot/controller.py
@@ -1,7 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String, Bool
-from geometry_msgs.msg import Twist, PoseStamped
+#from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import Twist, PoseStamped #, TransformStamped
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 import transforms3d
@@ -17,54 +18,74 @@ class ControllerClass(Node):
         self.y = 2.7
         self.theta = 0.0
         self.Kp_linear = 0.1
-        self.Kp_angular = 0.2
+        self.Kp_angular = 0.3
         self.max_linear_speed = 0.3
         self.max_angular_speed = 0.3
         self.follow_distance = 0.8
         self.stop_d = 0.05
-        self.turning_d = np.deg2rad(10)
+        self.turning_d = np.deg2rad(5)
         self.follow = False
         self.lidar_recieved = False
+        #self.tf_br2 = TransformBroadcaster(self)
+        #self.t = TransformStamped()
         self.lidar = LaserScan()
+        #self.goal_lidar = LaserScan()
         self.robot_vel = Twist()
         self.next_point = Bool()
         self.inf1 = String()
+        self.inf2 = String()
+        self.ll = []
+        self.zero = (np.zeros(360, dtype=np.float32) + 0.05).tolist()
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.sub_p = self.create_subscription(Odometry, 'robot_pose', self.pose_callback, 10)
         self.sub_g = self.create_subscription(PoseStamped, 'set_point', self.goal_callback, 10)
         self.sub_l = self.create_subscription(LaserScan, "scan", self.lidar_callback, 10)
+        #self.pub_l = self.create_publisher(LaserScan, 'goal_scan', 10)
         self.pub_v = self.create_publisher(Twist, 'cmd_vel', 10)
         self.pub = self.create_publisher(String, 'inf1', 10)
+        self.pub2 = self.create_publisher(String, 'inf2', 10)
         self.start_time = self.get_clock().now()
 
     def get_view(self, r, center, limit, max_distance):
+        #self.goal_lidar.ranges = self.zero
+        self.ll = []
         detect = []
         i_i = center - limit
         i_f = center + limit
         if i_i < 0:
             for i in range(0, i_f):
+                self.ll.append(r[i])
+                #self.goal_lidar.ranges[i] = r[i]
                 if r[i] > max_distance:
                     detect.append(0)
                 else:
                     detect.append(1)
             for i in range(360 - i_i, 360):
+                self.ll.append(r[i])
+                #self.goal_lidar.ranges[i] = r[i]
                 if r[i] > max_distance:
                     detect.append(0)
                 else:
                     detect.append(1)
         elif i_f > 360:
             for i in range(i_i, 360):
+                self.ll.append(r[i])
+                #self.goal_lidar.ranges[i] = r[i]
                 if r[i] > max_distance:
                     detect.append(0)
                 else:
                     detect.append(1)
             for i in range(0, i_f - 360):
+                self.ll.append(r[i])
+                #self.goal_lidar.ranges[i] = r[i]
                 if r[i] > max_distance:
                     detect.append(0)
                 else:
                     detect.append(1)
         else:
             for i in range(i_i, i_f):
+                self.ll.append(r[i])
+                #self.goal_lidar.ranges[i] = r[i]
                 if r[i] > max_distance:
                     detect.append(0)
                 else:
@@ -86,6 +107,7 @@ class ControllerClass(Node):
 
     def lidar_callback(self, msg2):
         self.lidar = msg2
+        #self.goal_lidar = msg2
         self.lidar_recieved = True
 
     def timer_callback(self):
@@ -97,7 +119,7 @@ class ControllerClass(Node):
             error_angle = np.arctan2(np.sin(error_angle), np.cos(error_angle))
             index_goal = int(np.rint(np.rad2deg(error_angle) + 180))
             goal_view = self.get_view(self.lidar.ranges, index_goal, 10, error_distance)
-            obj_distance = min(self.lidar.ranges)
+            obj_distance = min(self.lidar.ranges[:180])
             a1 = 0
             a2 = 0
             if obj_distance < 999999:
@@ -105,6 +127,9 @@ class ControllerClass(Node):
                 a2 = a1 + 90
                 if obj_distance < 0.2:
                     a2 += 10
+                if obj_distance > 0.3:
+                    a2 -= 5
+                
             follow_angle = np.deg2rad(a2)
 
             if goal_view == "clear" or error_distance < obj_distance:
@@ -143,10 +168,27 @@ class ControllerClass(Node):
                 stopped = True
                 self.robot_vel.linear.x = 0.0
                 self.robot_vel.angular.z = 0.0
+            
+            '''
+            self.t.header.stamp = self.get_clock().now().to_msg()
+            self.t.header.frame_id = 'base_footprint'
+            self.t.child_frame_id = 'goal'
+            self.t.transform.translation.x = error_distance * np.cos(error_angle)
+            self.t.transform.translation.y = error_distance * np.cos(error_angle)
+            self.t.transform.translation.z = 0.0
+            q = transforms3d.euler.euler2quat(0, 0, error_angle)
+            self.t.transform.rotation.x = q[1]
+            self.t.transform.rotation.y = q[2]
+            self.t.transform.rotation.z = q[3]
+            self.t.transform.rotation.w = q[0]
+            self.tf_br2.sendTransform(self.t)
+            '''
+
             self.pub_v.publish(self.robot_vel)
             self.inf1.data = str(
                 str(goal_view == "obstacle") + " " +
                 str(self.follow) + " " +
+                "ind:" + str(index_goal) + " " +
                 "o_d:" + str(np.round(obj_distance, 2)) + " o_th:" + str(a1) + " " +
                 "f_th:" + str(a2) + " " +
                 "x:" + str(np.round(self.x, 2)) + " y:" + str(np.round(self.y, 2)) + " th:" + str(np.round(np.rad2deg(self.theta), 2)) + " " +
@@ -155,7 +197,10 @@ class ControllerClass(Node):
                 str(goal_reached) + " " +
                 str(stopped)
             )
+            self.inf2.data = str(np.round(np.array(self.ll), 2)) +  " " + str(np.round(error_distance, 2)) + " " + str(np.round(np.rad2deg(error_angle), 2)) + " " + str(index_goal) + " " + str(goal_view)
             self.pub.publish(self.inf1)
+            self.pub2.publish(self.inf2)
+            #self.pub_l.publish(self.goal_lidar)
 
 def main(args=None):
     rclpy.init(args=args)
